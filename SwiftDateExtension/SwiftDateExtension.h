1a9ca4cb35ad2f879f88ca7e54b916dfb8f6042a
Alex Leite <admin@al7dev.com>
2017-02-15 13:56:00 -0600

    Creating framework target

---
+++ SwiftDateExtension/SwiftDateExtension.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDateExtension.h
+//  SwiftDateExtension
+//
+//  Created by Alexandre Leite on 2/15/17.
+//  Copyright © 2017 al7dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftDateExtension.
+FOUNDATION_EXPORT double SwiftDateExtensionVersionNumber;
+
+//! Project version string for SwiftDateExtension.
+FOUNDATION_EXPORT const unsigned char SwiftDateExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDateExtension/PublicHeader.h>
+
+
